4e2274bf878821ee62f49e92250d4109194aee2c
Igorfilippov444 <31948830+IgorFilippov444@users.noreply.github.com>
2018-04-24 11:21:21 +0300

    tehtud

---
+++ Pendel.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author JustIF
+ */
+import java.lang.*;
+public class Pendel {
+	public static void main(String[] args){
+	double pikkus;
+	double Vonkeperiood;
+	pikkus = 5;
+	Vonkeperiood = 2*(Math.PI)*(Math.sqrt(pikkus/9.8));
+	System.out.println(Vonkeperiood);
+	
+		
+	}
+}
