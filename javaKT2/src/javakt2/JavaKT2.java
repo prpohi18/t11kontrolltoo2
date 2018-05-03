5ebc027f8cf6cf94c706bc82f8786156c12f95e1
Kaur K <kaurk@users.noreply.github.com>
2018-05-03 09:00:27 +0300

    Ülesande püstitus

---
+++ javaKT2/src/javakt2/JavaKT2.java
@@ -0,0 +1,14 @@
+/*
+Loo klass Kolmkõla mazoorkolmkõla nootide arvutamiseks. Konstruktoris antakse sisse täisarvuline põhitoon, eraldi käsuga väljastatakse põhitoon, suur terts (+4) ja kvint (+7). 
+Ehk siis kui põhitooniks on 60(C), siis väljastatakse arvud 60 64 67. Loo toimimise kontrollimiseks automaattest.
+*/
+
+package javakt2;
+
+public class JavaKT2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
