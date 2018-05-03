0e954eeaaa5470321741c4760e03d0814850cf43
Krislyn Luks <krsln@tlu.ee>
2018-05-03 09:55:38 +0300

    vale test

---
+++ GeeniTest.java
@@ -0,0 +1,36 @@
+package krislyn.kontrolltoo.test;
+
+
+public class GeeniTest {
+    
+    @RequestMapping
+    public void contextLoads(){}
+/*
+    @Test
+    public void alleeliTest(){
+        Alleelid alleel1 = new Alleelid();
+        Assert.assertNull(alleel1.getName());
+        Assert.assertNull(alleel1.getPositive());
+        alleel1.setName("Reesus");
+        alleel1.setPositive(true);
+        Assert.assertEquals("Reesus", alleel1.getName());
+        Assert.assertEquals(true, alleel1.getPositive());
+    }
+
+    @Test
+    public void geeniTestidAuto(){
+        Alleelid alleel1 = new Alleelid();
+        Alleelid alleel2 = new Alleelid();
+        alleel1.setName("Reesus");
+        alleel1.setPositive(false);
+        alleel2.setName("RhNegatiivne");
+        alleel2.setPositive(true);
+        Geenid Geenid = new Geenid(alleel1, alleel2);
+        Assert.assertEquals(true, Geenid.getPositive());
+        Assert.assertEquals(false, Geenid.getAlleel1().getPositive());
+        Assert.assertEquals("RhNegatiivne", Geenid.getAlleel2().getName());
+        Geenid.getAlleel2().setPositive(false);
+        Geenid.setPositive();
+        Assert.assertEquals(false, Geenid.getPositive());
+    } */
+}
