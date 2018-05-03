f7afeabeae45d30c617f62bde7c21e3500b76435
Kaur K <kaurk@users.noreply.github.com>
2018-05-03 09:58:23 +0300

    Klasside loomine. Terts klass on korras

--- javaKT2/src/javakt2/JavaKT2.java
+++ javaKT2/src/javakt2/JavaKT2.java
@@ -5,10 +5,20 @@ Ehk siis kui põhitooniks on 60(C), siis väljastatakse arvud 60 64 67. Loo toim
 
 package javakt2;
 
-public class JavaKT2 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class JavaKT2 {
     
+    
+    public static void main(String[] args) {
+// Here will be program that gets input and gives  3-sound    
+     Scanner name = new Scanner(System.in);
+        System.out.println("Sisesta p6hitoon (1-127)");
+        String toon = name.nextLine();
+        int nr = Integer.parseInt(toon); //string to int
+   // System.out.println(nr);    
+    terts t1 = new terts(nr);
+    
+    System.out.println("P6hitoon on "+nr+", terts on "+t1.value);
+    }
 }
