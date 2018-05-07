c889ef7be694a8b1c6f47b0f64d9efba7c26415b
Veli Vaigur <veliva@users.noreply.github.com>
2018-05-07 10:34:22 +0300

    kt 2 punkti valmis

---
+++ src/main/java/kt2/Toidukomponent.java
@@ -0,0 +1,17 @@
+package kt2;
+
+public class Toidukomponent{
+    double kogus;
+    Toiduaine sisu;
+    public Toidukomponent(double kogus, Toiduaine sisu) {
+        this.kogus = kogus;
+        this.sisu = sisu;
+    }
+    public double rasvakogus() {
+        return (kogus * (sisu.rasvad / 100));
+    }
+    @Override
+    public String toString() {
+        return "kogus: " + kogus + " g" +", sisu: " + sisu + ", rasvakogus: " + rasvakogus();
+    }
+}
