86bea05a17bec0b7b51ecc64e4cfc5e093be6678
Julika Maiste <julimai@users.noreply.github.com>
2018-05-07 11:43:56 +0300

    teine punkt valmis

--- src/test/java/tahed/TaheTest.java
+++ src/test/java/tahed/TaheTest.java
@@ -19,3 +19,35 @@ public class TaheTest{
 		
 	}
 }
+/* Veebitest.java käivitatud NetBeansis ja tulemus oli positiivne.
+
+kood:
+
+package tahed;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import java.util.HashSet;
+import java.util.Set;
+import org.openqa.selenium.By;
+
+public class Veebitest {
+    public static void main(String[] args){ 
+        open("http://greeny.cs.tlu.ee:40305/tahed.html");
+        $("#kast").setValue("kaalikas ja kapsas on juurviljad");
+        $("#nupp").click();
+        System.out.println($("#vastus").getText());
+    }
+}
+
+Tulemus:
+
+Sisestatud lause: kaalikas ja kapsas on juurviljad
+'A' tähti selles lauses: 7
+Sõnade arv lauses: 5
+'A' tähti igas sõnas:
+(3)kaalikas
+(1)ja
+(2)kapsas
+(0)on
+(1)juurviljad*/
