8b718bf19f22854881d3784e90c2b0305208b251
kurmo <kurmo14@tlu.ee>
2018-05-30 18:26:15 +0300

    final

---
+++ src/main/java/kt2/Toiduaine.java
@@ -0,0 +1,22 @@
+package kt2;
+
+public class Toiduaine {
+    public String nimetus;
+    public double valgud;
+    public double rasvad;
+    public double susivesikud;
+    public Toiduaine(String nimetus, double valgud, double rasvad, double susivesikud){
+        this.nimetus = nimetus;
+        this.valgud = valgud;
+        this.rasvad = rasvad;
+        this.susivesikud = susivesikud;
+    }
+    @Override
+    public String toString() {
+        if ((rasvad + susivesikud) > 100) {
+            return "Protsent ei saa olla üle saja!";
+        } else {
+            return nimetus + ", " + valgud + " g, " + rasvad + " %, " + susivesikud + " %";
+        }
+    }
+}
