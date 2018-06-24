d58287eace05d05fc6537dc1eb15284b1768941a
Peeter Roop <31948857+petsens@users.noreply.github.com>
2018-06-24 15:53:53 +0300

    kontrolltöö2

---
+++ ioonid/src/test/java/IooniTest.java
@@ -0,0 +1,16 @@
+import ee.tlu.peeterr.ioonid.Ioon;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class IooniTest {
+    public IooniTest() {
+    }
+    @Test
+    public void loomine(){
+        Ioon i1 = new Ioon("H+", 1, 1);
+        Ioon i2 = new Ioon ("NO3-", 3, -1);
+        
+        assertEquals(1, i1.kysiLaeng());
+        assertEquals(3, i2.kysiAatommass());
+    }
+    
+}
